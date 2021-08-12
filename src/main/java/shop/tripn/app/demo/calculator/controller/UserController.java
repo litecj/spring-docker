a12d32db8093971bc2b644f4abaa36eda6952941
litecj <litecj702@gmail.com>
2021-08-12 16:20:50 +0900

    wow

--- src/main/java/shop/tripn/app/demo/calculator/controller/UserController.java
+++ src/main/java/shop/tripn/app/demo/calculator/controller/UserController.java
@@ -3,6 +3,7 @@ package shop.tripn.app.demo.calculator.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.tripn.app.demo.calculator.entity.User;
 import shop.tripn.app.demo.calculator.service.UserServiceImpl;
@@ -10,7 +11,8 @@ import shop.tripn.app.demo.calculator.service.UserServiceImpl;
 import java.util.List;
 import java.util.Optional;
 
-@RestController@RequiredArgsConstructor
+@RestController @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserServiceImpl userService;
 
